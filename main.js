185f31435b67266b3d08b8e18eac21e28bd78f7f
samy-c <samycc777@gmail.com>
2022-10-12 22:55:39 +0200

    Add playSingleRound(playerSelection, computerSelection)

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 function getComputerChoice() {
   // Returns a random integer from 0 to 2
   let nb = Math.floor(Math.random() * 3);
+  let choice;
 
-  if (nb == 0) return "Rock";
-  else if (nb == 1) return "Paper";
-  else return "Scissors";
+  if (nb == 0) choice = "Rock";
+  else if (nb == 1) choice = "Paper";
+  else choice = "Scissors";
+
+  return choice;
+}
+
+function playSingleRound(playerSelection, computerSelection) {
+  playerSelection =
+    playerSelection[0].toUpperCase() + playerSelection.toLowerCase().slice(1);
+
+  let result;
+
+  if (playerSelection == computerSelection) 
+    result = "Draw";
+  else if ((playerSelection == "Rock" && computerSelection == "Scissors")||(playerSelection == "Scissors" && computerSelection == "Paper")||(playerSelection == "Paper" && computerSelection == "Rock")) 
+    result = `You Win! ${playerSelection} beats ${computerSelection}`;
+  else
+    result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+  
+    return result;
 }
